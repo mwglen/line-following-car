6ee6b8260ceca0339f09aac34e9c7d7ed97fe721
Matt Glen <mwg2202@yahoo.com>
2022-03-07 18:56:04 -0500

    Partial conflict resolution for #3

--- led.c
+++ led.c
@@ -1,9 +1,11 @@
+/// Includes
 #include "msp430.h"
 #include "ports.h"
 
 /// Functions
 void Init_LEDs(void){
-  P1OUT |=  RED_LED; // Turn off Red LED
-  P6OUT &= ~GRN_LED; // Turn on Green LED
+// Turns on both LEDs
+  P1OUT &= ~RED_LED;
+  P6OUT |= GRN_LED;
 }
 
