ee5f87a6df9790bf29b69454dce2ae6366f22235
Matt Glen <mwg2202@gmail.com>
2022-04-24 23:25:10 -0700

    Fixed errors from when code was reorganized

--- src/main.c
+++ src/main.c
@@ -41,19 +41,9 @@ void main(void){
   init_wheels();
   init_timer_B0();
   init_adc();
-<<<<<<< HEAD:src/main.c
-
-  // Update display for project 8
-  strcpy(display_line[0], " Waiting! ");
-  strcpy(display_line[2], "BR:       ");
-  display_changed = true;
-
-  // Clear Display  
-=======
   init_pc(4, 0x5551); //115200 
   init_iot(4, 0x5551); //115200
    
->>>>>>> b117484771621bb0e6ac1b03d80fe6db86e19d5f:main.c
   while(true) {
     
     // Run Program
@@ -63,51 +53,13 @@ void main(void){
     // backwords at the same time
     check_wheels();
     
-<<<<<<< HEAD:src/main.c
-    // Send transmission for Homework 8 
-    if (send_transmission && (PROGRAM_COUNT >= TIME_2_SECS)) {
-
-       // Put a string into transmission global
-       strcpy(transmission, "ABCDEFGHIJ");
-
-       // Put other info on display
-       strcpy(display_line[0], " Transmit ");
-       strcpy(display_line[1], transmission);
-
-       // Enable transmission interrupt
-       UCA0IE |= UCTXIE;
-       
-       recieve_index = 0;
-       
-       send_transmission = false;
-    }
-
-    // Display recieved messages for Homework 8
-    if (message_recieved) {
-
-       // Say that you've handled the message
-       message_recieved = false;
-
-       // Put other info on display
-       strcpy(display_line[0], " Recieved ");
-       
-       // Put message on display
-       for (int i = 0; i < 10; i++)
-          display_line[3][i] = recieved_message[i];
-
-       // Update the display
-       display_changed = true;
-    }
-    
-    display_process();   // Update Display
-=======
     // Run processes
     adc_process();
     display_process();
     wheels_process();
     iot_process();
     pc_process();
->>>>>>> b117484771621bb0e6ac1b03d80fe6db86e19d5f:main.c
+
     P3OUT ^= TEST_PROBE; // Change State of TEST_PROBE OFF
   }
 }
